20e98cf999ce1fe243c8a9349d30c0519774c3f5
Soulyaris <genessa174@gmail.com>
2019-05-25 17:00:48 +0500

    added 10% opacity change and new theme

--- script/script.js
+++ script/script.js
@@ -24,10 +24,12 @@ function fieldSizeLabelChange(){
 
 function fillGameField() {
   gameField.innerHTML = "";
-  var cellWidth = (gameField.offsetWidth - 52) / fieldSize.value;
-  var cellHeight = (gameField.offsetHeight - 52) / fieldSize.value
-  for (var i = 0; i < fieldSize.value * fieldSize.value; i++) 
-    gameField.innerHTML += `<div class="fieldCell" style="width:${cellWidth-2}px; height:${cellHeight-2}px; min-height: 1px">&nbsp</div>`;
+  gameField.style.gridTemplateRows = `repeat(${fieldSize.value}, 1fr)`;
+  gameField.style.gridTemplateColumns = `repeat(${fieldSize.value}, 1fr)`;
+  for (var i = 0; i < fieldSize.value ** 2; i++) {
+    let  fieldCell = document.createElement('div');
+    fieldCell.classList.add('fieldCell');
+    gameField.appendChild(fieldCell);};
   generateCellEvents();
 }
 
@@ -35,15 +37,26 @@ function generateCellEvents(){
   var fieldCells = document.querySelectorAll('.fieldCell');
   fieldCells.forEach((cell) => {
     cell.addEventListener('mouseover', (e) => {
-      var colorScheme = document.querySelector('input[name=colorScheme]:checked').value;
-      switch(true) {
-        case colorScheme == "black" : cell.style.backgroundColor = "black"; break;
-        case colorScheme == "rainbow" : {
-          cell.style.backgroundColor = `rgb(${colorIntensity()}, ${colorIntensity()}, ${colorIntensity()})`;
-        } break;
-      }
+      applyNewColor(cell);
     });
   });
+}
+
+function applyNewColor(cell){
+  var colorScheme = document.querySelector('input[name=colorScheme]:checked').value;
+  switch(true) {
+    case colorScheme == "black" : cell.style.backgroundColor = `rgba(255,255,255,0)`; break;
+    case colorScheme == "rainbow" : {
+      cell.style.backgroundColor = `rgba(${colorIntensity()}, ${colorIntensity()}, ${colorIntensity()}, 1)`;
+    } break;
+    case colorScheme == "+10% black" : {
+      var alphaColor = getComputedStyle(cell).getPropertyValue("background-color");
+      var parts = alphaColor.match(/[\d.]+/g);
+      if (parts.length === 3) parts.push(1);
+      parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) - 0.1));
+      cell.style.backgroundColor = `rgba(${ parts.join(',') })`;
+    } break;
+  }
 }
 
 function colorIntensity() {
