5ff70097956e109da542fdf3431d7c9750d78c5e
Soulyaris <genessa174@gmail.com>
2019-05-27 13:59:24 +0500

    small script update

--- script/script.js
+++ script/script.js
@@ -26,7 +26,7 @@ function fillGameField() {
   gameField.innerHTML = "";
   gameField.style.gridTemplateRows = `repeat(${fieldSize.value}, 1fr)`;
   gameField.style.gridTemplateColumns = `repeat(${fieldSize.value}, 1fr)`;
-  for (var i = 0; i < fieldSize.value ** 2; i++) {
+  for (let i = 0; i < fieldSize.value ** 2; i++) {
     let  fieldCell = document.createElement('div');
     fieldCell.classList.add('fieldCell');
     gameField.appendChild(fieldCell);};
@@ -34,7 +34,7 @@ function fillGameField() {
 }
 
 function generateCellEvents(){
-  var fieldCells = document.querySelectorAll('.fieldCell');
+  let fieldCells = document.querySelectorAll('.fieldCell');
   fieldCells.forEach((cell) => {
     cell.addEventListener('mouseover', (e) => {
       applyNewColor(cell);
@@ -43,15 +43,15 @@ function generateCellEvents(){
 }
 
 function applyNewColor(cell){
-  var colorScheme = document.querySelector('input[name=colorScheme]:checked').value;
+  let colorScheme = document.querySelector('input[name=colorScheme]:checked').value;
   switch(true) {
     case colorScheme == "black" : cell.style.backgroundColor = `rgba(255,255,255,0)`; break;
     case colorScheme == "rainbow" : {
       cell.style.backgroundColor = `rgba(${colorIntensity()}, ${colorIntensity()}, ${colorIntensity()}, 1)`;
     } break;
     case colorScheme == "+10% black" : {
-      var alphaColor = getComputedStyle(cell).getPropertyValue("background-color");
-      var parts = alphaColor.match(/[\d.]+/g);
+      let alphaColor = getComputedStyle(cell).getPropertyValue("background-color");
+      let parts = alphaColor.match(/[\d.]+/g);
       if (parts.length === 3) parts.push(1);
       parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) - 0.1));
       cell.style.backgroundColor = `rgba(${ parts.join(',') })`;
@@ -59,8 +59,4 @@ function applyNewColor(cell){
   }
 }
 
-function colorIntensity() {
-  var rand = 0 - 0.5 + Math.random() * (255 - 0 + 1)
-  rand = Math.round(rand);
-  return rand;
-}
+let colorIntensity = () => Math.round(0 - 0.5 + Math.random() * (255 - 0 + 1));
